fc66e038354bf3bd9796fd012a3b47f6719b7ba6
conner fortner <fortner.conner@yahoo.com>
2020-09-15 21:01:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  nintendo_games=["zelda", "mario", "smashbros", "animalcrossing"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  nintendo_games 
 end
 
 def add_element_to_start_of_array(array, element)
